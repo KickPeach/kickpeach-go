0ec19f7423a13ed6240d774f4a72d69bdc9a67c6
shisiying <shisiying@xhzyxed.cn>
2020-06-21 18:54:36 +0800

    feat:group router

--- main.go
+++ main.go
@@ -11,17 +11,30 @@ func main() {
 		c.HTML(http.StatusOK, "<h1>Hello kickpeach</h1>")
 	})
 
-	r.GET("/hello", func(c *kickpeach.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
+	v1 := r.Group("/v1")
 
-	r.GET("/hello/:name", func(c *kickpeach.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
+	{
+		v1.GET("/", func(c *kickpeach.Context) {
+			c.HTML(http.StatusOK, "<h1>Hello kickpeach</h1>")
+		})
 
-	r.GET("/assets/*filepath", func(c *kickpeach.Context) {
-		c.JSON(http.StatusOK, kickpeach.H{"filepath": c.Param("filepath")})
-	})
+		v1.GET("/hello", func(c *kickpeach.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.GET("/hello/:name", func(c *kickpeach.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.POST("/login", func(c *kickpeach.Context) {
+			c.JSON(http.StatusOK, kickpeach.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+
+	}
 
 	r.Run(":9999")
 }
